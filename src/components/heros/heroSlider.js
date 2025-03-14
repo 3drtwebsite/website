e953e21373a45185ab5e7ff5e423a55e82b1a7ad
Windsor Fleet <et725560@dal.ca>
2025-03-14 18:24:21 -0300

    Changed homepage to contain a background video

--- src/components/heros/heroSlider.js
+++ src/components/heros/heroSlider.js
@@ -1,135 +1,75 @@
 import React from "react"
 import styled from "styled-components"
 import { ButtonPrimary, ButtonSecondaryLight } from "../buttons"
-import { Actions } from "../layoutComponents"
-import { StaticImage } from "gatsby-plugin-image"
 import "bootstrap/dist/css/bootstrap.min.css"
-import { Carousel } from "react-bootstrap"
-import { Autoplay } from "swiper"
+import videoSrc from "../videos/BackgroundVideo_3DRT.mp4"
 
-const Wrapper = styled.div`
-  padding-bottom: 4em;
-  p {
-    margin-bottom: 0;
-  }
-
-  video {
-    height: 500px;
-    object-fit: cover;
-  }
-`
-
-const Content = styled.div`
+const Wrapper = styled.section`
   position: relative;
-
-  &:before {
-    content: "";
-    position: absolute;
-    background: rgba(0, 0, 0, 0.1);
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-  }
+  width: 100%;
+  height: 100vh;
+  overflow: hidden;
 `
 
-const Content1 = styled.div`
-  background: url("../../images/slider-1.jpg");
-  /* background-blend-mode: overlay; */
-  background-position: center;
-  height: 100vh;
-  background-size: cover;
-  background-repeat: no-repeat;
-`
-const Content2 = styled.div`
-  background: url("../../images/slider-2.jpg");
-  /* background-blend-mode: overlay; */
-  background-position: center;
-  height: 100vh;
-  background-size: cover;
-  background-repeat: no-repeat;
-`
-const Content3 = styled.div`
-  background: url("../../images/slider-3.jpg");
-  /* background-blend-mode: overlay; */
-  background-position: center;
-  height: 100vh;
-  background-size: cover;
-  background-repeat: no-repeat;
+const VideoBackground = styled.video`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  z-index: 1;
 `
 
-const HeroTitle = styled.h2`
+const Overlay = styled.div`
+  position: relative;
+  z-index: 2;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  height: 100%;
+  padding: 2rem;
+  color: #fff;
+  text-shadow: -2px 2px 4px rgba(0, 0, 0, 0.7);
+  background: rgba(0, 0, 0, 0.3);
+`
+
+const HeroTitle = styled.h1`
   font-size: var(--fs-title);
+  margin-bottom: 1rem;
+  text-transform: uppercase;
 `
 
-const CaptionInner = styled.div`
-  // background: rgba(0, 0, 0, 0.3);
-  // padding: 1em;
-  // border-radius: var(--br);
+const HeroSubtitle = styled.p`
+  font-size: var(--fs-3);
+  margin-bottom: 1.5rem;
+  text-transform: uppercase;
 `
 
-const captionStyle = {
-  textAlign: "left",
-  display: "flex",
-  alignItems: "center",
-  height: "90%",
-  left: "12%",
-  color: "var(--txt-light)",
-  textShadow: "-3px 3px 3px #000000",
-  fontSize: "var(--fs-3)",
-}
-
-export default function HeroSlider(props) {
+export default function HeroSlider() {
   return (
     <Wrapper>
-      <Carousel>
-        <Carousel.Item interval={5000}>
-          <Content>
-            <Content1 className="d-block w-100" />
-          </Content>
-          <Carousel.Caption style={captionStyle} className="spacing">
-            <div>
-              <HeroTitle className="upper">3D Printing</HeroTitle>
-              <p className="upper">
-                Our in-house rapid prototyping and digital production
-                capabilities are ready to meet your unique manufacturing
-                challenges!
-              </p>
-              <hr />
-            </div>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item interval={5000}>
-          <Content>
-            <Content2 className="d-block w-100" />
-          </Content>
-          <Carousel.Caption style={captionStyle} className="spacing">
-            <div>
-              <HeroTitle className="upper">Digital Design</HeroTitle>
-              <p className="upper">
-                We specialize in Design for Additive Manufacturing (DFAM), and
-                optimizing geometries for 3D print and digital production.
-              </p>
-              <hr />
-            </div>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item interval={5000}>
-          <Content>
-            <Content3 className="d-block w-100" />
-          </Content>
-          <Carousel.Caption style={captionStyle} className="spacing">
-            <CaptionInner>
-              <HeroTitle className="upper">3D Fusion Manufacturing</HeroTitle>
-              <p className="upper">
-                Combined with 3D Printing, 3D Fusion Manufacturing opens doors
-                to cost effective, scalable, just-in-time production!
-              </p>{" "}
-              <hr />
-            </CaptionInner>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
+      <VideoBackground
+        autoPlay
+        loop
+        muted
+        playsInline
+      >
+        <source src={videoSrc} type="video/mp4" />
+        Your browser does not support the video tag.
+      </VideoBackground>
+      <Overlay>
+        <HeroTitle>3D Printing</HeroTitle>
+        <HeroSubtitle>
+          Our in-house rapid prototyping and digital production capabilities are
+          ready to meet your unique manufacturing challenges!
+        </HeroSubtitle>
+        <div style={{ display: "flex", gap: "1rem" }}>
+          <ButtonPrimary>Form Button</ButtonPrimary>
+          <ButtonSecondaryLight>Phone Button</ButtonSecondaryLight>
+        </div>
+      </Overlay>
     </Wrapper>
   )
 }
